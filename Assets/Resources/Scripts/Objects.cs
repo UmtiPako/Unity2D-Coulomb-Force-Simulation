7b2998a719e918280a1854cf7f4814a72939147e
Umut Paklacı <71903884+UmtiPako@users.noreply.github.com>
2025-02-23 03:51:39 +0300

    Initial commit

---
+++ Assets/Resources/Scripts/Objects.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objects", menuName = "Scriptable Objects/Objects")]
+public class Objects : ScriptableObject
+{
+    public ChargePanel panel;
+    public ChargeManager manager;
+}
