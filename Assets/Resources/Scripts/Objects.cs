e2b4f1dc80c4ca33df006b49fef24e944d02a9f5
Umut Paklacı <71903884+UmtiPako@users.noreply.github.com>
2025-02-23 03:42:15 +0300

    Removed ignored files

--- Assets/Resources/Scripts/Objects.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Objects", menuName = "Scriptable Objects/Objects")]
-public class Objects : ScriptableObject
-{
-    public ChargePanel panel;
-    public ChargeManager manager;
-}
